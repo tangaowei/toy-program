0ad8180ccdd2419625329c3eb3dacd135d71471c
tangaowei <tangaowei@baidu.com>
2016-11-06 17:15:36 +0800

    change: add permutation problem

---
+++ leetcode/Permutations.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        return recur(0, nums);
+    }
+
+    private List<List<Integer>> recur(int index, int[] nums) {
+        if(index == nums.length - 1) {
+            List<Integer> list = new LinkedList<Integer>();
+            list.add(nums[index]);
+            List<List<Integer>> llist = new LinkedList<List<Integer>>();
+            llist.add(list);
+            return llist;
+        }
+        List<List<Integer>> llist = new LinkedList<List<Integer>>();
+        for(int i = index; i < nums.length; ++i) {
+            swap(nums, index, i);
+            List<List<Integer>> tmpLlist = recur(index + 1, nums);
+            for(List<Integer> tmplist : tmpLlist) {
+                tmplist.add(0, nums[index]);
+                llist.add(tmplist);
+            }
+            swap(nums, index, i);
+        }
+        return llist;
+    }
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        Permutations obj = new Permutations();
+        int[] nums = {4, 1, 2, 3};
+        List<List<Integer>> llist = obj.permute(nums);
+        for(List<Integer> list : llist) {
+            System.out.println(list.toString());
+        }
+    }
+}
